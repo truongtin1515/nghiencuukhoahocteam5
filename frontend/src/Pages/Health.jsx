adf7f64d9795bb84f992e48dbfc7d5b98f07812a
truongtin1515 <111273656+truongtin1515@users.noreply.github.com>
2025-03-13 02:55:39 +0700

    Delete frontend directory

--- frontend/src/Pages/Health.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Health = () => {
-  return (
-    <div>Health</div>
-  )
-}
-
-export default Health
--- frontend/src/Pages/HomeCategory.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HomeCategory = () => {
-  return (
-    <div>HomeCategory</div>
-  )
-}
-
-export default HomeCategory
